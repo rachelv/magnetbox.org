43755845715e19de9c0810fbe96d2c8682759f5f
Rachel Vecchitto <rachel@magnetbox.org>
2023-04-04 20:20:35 -0600

    resume updates, start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["index.html"],
+  content: [
+    '.index.html',
+    './resume/index.html'
+  ],
   theme: {
     extend: {
       fontFamily: {
